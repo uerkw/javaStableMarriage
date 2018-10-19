380aa3d711a5e0ff38657a5109a3d78d288515cd
KyleUerkwitz <kyleuerkwitz@gmail.com>
2018-10-19 16:21:35 -0400

    Changes method 'makePairData' to 'makeRawData', clarifying usage. Reads file and produces usable data for main program.

--- src/LDS_Project1/FileUtilities.java
+++ src/LDS_Project1/FileUtilities.java
@@ -4,8 +4,12 @@ import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.Arrays;
+
+// EECS 2500, Project1
+// Written by Kyle Uerkwitz
 
 /**
  *  A collection of useful methods for reading and creating data from
@@ -13,10 +17,10 @@ import java.util.Arrays;
  */
 class FileUtilities {
     // Use Colors from beginning of Project1.java
-    private static String ANSI_RED = Project1.ANSI_RED;
-    private static String ANSI_YELLOW = Project1.ANSI_YELLOW;
-    private static String ANSI_CYAN = Project1.ANSI_CYAN;
-    private static String ANSI_WHITE = Project1.ANSI_WHITE;
+    private static final String ANSI_RED = Project1.ANSI_RED;
+    private static final String ANSI_YELLOW = Project1.ANSI_YELLOW;
+    private static final String ANSI_CYAN = Project1.ANSI_CYAN;
+    private static final String ANSI_WHITE = Project1.ANSI_WHITE;
 
 
     /**
@@ -48,7 +52,7 @@ class FileUtilities {
      *  creating a reader and handling the exceptions associated with it.
      *
      * @param filePath  the relative or absolute path giving the location of the test file
-     * @return          the formed bufferedReader of the file
+     * @return          the formed bufferedReader linked to the file
      */
     static BufferedReader assignReader(String filePath){
         BufferedReader bufferedReader = null;
@@ -62,21 +66,31 @@ class FileUtilities {
         }
         return bufferedReader;
     }
-    static ArrayList makePairData(int pairingSize){
 
-        //TODO Create method to create usable array from pairing size
-        //Use 'pairingSize' var to create two arrays of size 'pairingSize'
-            //Array 1 = Senior Programmers
-            //Array 2 = Junior Programmers
-            //Each element in array is another array, one to hold NAME, other to hold PREFERENCES
-        //Use 'For' loop with iterations equal to pairing size to fill each of JR/SR array data
-            //Create a subarray for each element. Subarray(0) = String Name, Subarray(1) = int preferences
-            //Read preferences, store in Subarray(1) as ints
-
-        //TODO Decide: ArrayLists or Arrays better? Arrays can only hold one type, ArrayLists can hold any.
-            // ArrayLists provide more functionality, easier to manipulate
-
-        ArrayList dataArray = null;
+    /**
+     *  Creates pairing data by reading the given file to parse the information into a usable format.
+     *  Modifying this method will allow the use of other input formats
+     *
+     * @param reader        a created bufferedReader so the method can start after reading the pairing
+     *                      size, and knows what file to read from
+     * @param pairingSize   an integer representing the pairingSize desired. Changing pairing size allows
+     *                      larger data sets to be used
+     * @return              an array of type 'individualData'. This lets all of the raw data be passed back to
+     *                      the origin of the method call.
+     * @throws IOException  If the reader cannot access the input file
+     */
+    static individualData[] makeRawData(BufferedReader reader, int pairingSize) throws IOException{
+        individualData[] dataArray = new individualData[pairingSize*2];
+        for(int i = 0; i < (pairingSize*2); i++){
+           String tempName = reader.readLine();
+           String preferences = reader.readLine();
+           String[] splitPrefs = preferences.split("\t");
+           int[] intPrefs = new int[pairingSize];
+           for(int j = 0; j < splitPrefs.length; j++)
+               intPrefs[j] = Integer.parseInt(splitPrefs[j]);
+           individualData temporaryData = new individualData(tempName, intPrefs);
+           dataArray[i] = temporaryData;
+        }
 
         return dataArray;
     }
@@ -86,17 +100,21 @@ class FileUtilities {
      * and raw pairing data for debugging purposes. Uses cyan text to differentiate
      * from errors, exceptions, and desired data.
      *
-     * @param pairingSize   the desired pairing size for the
-     * @param pairingData   arrayList full of data from the input file
+     * @param pairingSize   the desired pairing size for the file
+     * @param pairingData   array full of data from the input file
      */
-    static void testCurrentData(int pairingSize, ArrayList pairingData){
+    static void testCurrentData(int pairingSize, individualData[] pairingData){
         System.out.println(ANSI_CYAN + "Pairing Size = " + pairingSize);
         System.out.println("Array Data:");
+        int dataSize = Array.getLength(pairingData);
         if (pairingData != null) {
-            for(int i = 0 ; i < pairingData.size(); i++){
-                System.out.println(ANSI_WHITE + "\t- " + pairingData.get(i));
+            for(int i = 0; i < dataSize; i++){
+                System.out.println(ANSI_WHITE + (pairingData[i].getName()));
+                for(int j = 0; j < pairingSize; j++ ) {
+                    System.out.print(ANSI_WHITE + (pairingData[i].getPreferences())[j] + " ");
+                }
+                System.out.println("");
             }
         }
-
     }
 }
